6066c8597f1e0a921036122acc5b10a4277c53ef
JoseOneTime <claveren@gmail.com>
2014-01-25 17:22:18 -0500

    Refactored sql example to use streaming API

--- sql/index.js
+++ sql/index.js
@@ -9,14 +9,23 @@ var connection = mysql.createConnection({
 	database : 'node'
 });
 
-connection.query(
-	'SELECT id, content FROM test',
-	function(err, results, fields){
-		if (err){
-			console.log(err);
-		} else {
-			console.log(results);
-		}
-		connection.end();
-	}
-);
+var query = connection.query('SELECT id, content FROM test');
+
+query.on('error', function(err){
+	console.log('A database error occurred: ');
+	console.log(err);
+});
+
+query.on('field', function(fields){
+	console.log('Received fields information.');
+});
+
+query.on('result', function(result){
+	console.log('Received result:');
+	console.log(result);
+});
+
+query.on('end', function(){
+	console.log	('Query execution has finished.');
+	connection.end();
+});
